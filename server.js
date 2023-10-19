608c664ef19c880d7ba1661c3dde3c37f8df4c7d
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-10-19 14:02:46 +0100

    Relized combine seed

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const Chance = require('chance');
+const { fakerDE, en } = require('@faker-js/faker');
+const { fakerEN } = require('@faker-js/faker');
+const { fakerPL } = require('@faker-js/faker');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const chance = new Chance();
 
 app.use(cors({ origin: '*' }));
 
+const host = "localhost";
 const port = process.env.PORT || 3001;
 
 let counter = 1;
@@ -53,12 +55,30 @@ function generateRandomCharacter() {
     return characters.charAt(randomIndex);
 }
 
-const generateFakeData = (errorCount) => {
+const generateFakeData = (errorCount, combinedSeed, locale) => {
     const number = counter++;
 
-    const name = chance.name();
-    const address = chance.address();
-    const phoneNumber = chance.phone();
+    let selectedFaker;
+    
+    switch (locale) {
+        case 'de':
+            selectedFaker = fakerDE;
+            break;
+        case 'en':
+            selectedFaker = fakerEN;
+            break;
+        case 'pl':
+            selectedFaker = fakerPL;
+            break;
+        default:
+            selectedFaker = fakerEN;
+    }
+
+    selectedFaker.seed(combinedSeed);
+
+    const name = selectedFaker.person.fullName();
+    const address = selectedFaker.location.streetAddress();
+    const phoneNumber = selectedFaker.phone.imei();
 
     return {
         number: number,
@@ -72,16 +92,57 @@ const generateFakeData = (errorCount) => {
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
+  
+    const params = new URLSearchParams(req.url.split('?')[1]);
+    const errorCount = parseFloat(params.get('errorCount') || 0);
+  
+    const seed = parseInt(params.get('seed'), 10);
+    const page = parseInt(params.get('page'), 10) || 1;
+    
+    const combinedSeed = seed + page;
+    const locale = params.get('region');
+  
+    const fakeData = generateFakeData(errorCount, combinedSeed, locale);
+    res.end(JSON.stringify(fakeData));
+  };  
 
+const itemsPerPage = 20;
+const maxPages = 100;
+
+app.get('/generateFakeData', (req, res) => {
     const params = new URLSearchParams(req.url.split('?')[1]);
     const errorCount = parseInt(params.get('errorCount'), 10);
+    const page = parseInt(params.get('page'), 10);
 
-    const fakeData = generateFakeData(errorCount);
-    res.end(JSON.stringify(fakeData));
-};
+    const seed = parseInt(params.get('seed'), 10);
 
-app.get('/api/generateFakeData', requestListener);
+    if (page >= 1 && page <= maxPages) {
+        const startIndex = (page - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+        const locale = params.get('region');
+
+        const fakeData = [];
+        for (let i = startIndex; i < endIndex; i++) {
+            console.log("st" + startIndex);
+            console.log("en" + endIndex);
+           
+            const combinedSeed = seed + page + i;
+            console.log(combinedSeed);
+            const item = generateFakeData(errorCount, combinedSeed, locale);
+
+            item.number = i+1;
+
+            fakeData.push(item);
+        }
+
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(fakeData);
+    } else {
+        res.status(400).json({ error: "Invalid page number" });
+    }
+});
+
+app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
 });
